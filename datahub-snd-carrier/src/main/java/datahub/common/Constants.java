2381f10af96e8657fe0dce2ab40a88e27b00b53a
takahashihiroto-f <takahashihiroto@fujitsu.com>
2025-03-24 19:05:05 +0900

    Create Constants.java

---
+++ datahub-snd-carrier/src/main/java/datahub/common/Constants.java
@@ -0,0 +1,14 @@
+package datahub.common;
+
+/**
+ * 定数クラス
+ * 
+ */
+public class Constants {
+
+	/** 
+	 * 共通エラーメッセージ 
+	 * 
+	 */
+	public static final String ERR_MSG = "JSON CSV変換バッチ処理で異常が発生しました。";
+}
